12a6714f8e5be0518008acd717f4f6f6417c421c
mayangindah <mayyeoja@gmail.com>
2019-10-22 14:26:06 +0700

    tugasprakweb2

---
+++ application/views/nama_saya.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>PRAKTIKUM<h1>
+<p>nama saya adalah mayang</p>
+<p>&nbsp;</p>
+</body>
+</html>
